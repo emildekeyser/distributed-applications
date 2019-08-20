2869e3ea4112c2e7444b23559f833814b1c16e4c
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-20 22:04:12 +0200

    exercises/basics/03-predicates

--- exercises/basics/03-predicates/tests.exs
+++ exercises/basics/03-predicates/tests.exs
@@ -4,33 +4,38 @@ file = System.get_env("DA_TESTFILE") || "student.exs"
 Code.load_file(file, __DIR__)
 
 
+defmodule Aux do
+  defmacro check(that: block, is_equal_to: expected) do
+    str = Macro.to_string(block)
+    quote do
+      test "#{unquote(str)} should be equal to #{unquote(expected)}" do
+        assert unquote(block) == unquote(expected)
+      end
+    end
+  end
+end
+
+
 defmodule Tests do
   use ExUnit.Case, async: true
+  import Aux
 
-  odd_numbers = [ 1, 3, 5, 99, -15 ]
-  even_numbers = [ 0, 2, 4, 6, 100, -40 ]
-
-  for n <- odd_numbers do
-    @n n
-
-    test "#{@n} is odd" do
-      assert Util.odd?(@n)
-    end
-
-    test "#{@n} is not even" do
-      assert not Util.even?(@n)
-    end
-  end
-
-  for n <- even_numbers do
-    @n n
-
-    test "#{@n} is not odd" do
-      assert not Util.odd?(@n)
-    end
-
-    test "#{@n} is even" do
-      assert Util.even?(@n)
-    end
-  end
+  check that: Util.odd?(1), is_equal_to: true
+  check that: Util.odd?(5), is_equal_to: true
+  check that: Util.odd?(7), is_equal_to: true
+  check that: Util.odd?(19), is_equal_to: true
+  check that: Util.odd?(-19), is_equal_to: true
+  check that: Util.odd?(0), is_equal_to: false
+  check that: Util.odd?(2), is_equal_to: false
+  check that: Util.odd?(4), is_equal_to: false
+  check that: Util.odd?(80), is_equal_to: false
+  check that: Util.odd?(-78), is_equal_to: false
+  check that: Util.even?(0), is_equal_to: true
+  check that: Util.even?(2), is_equal_to: true
+  check that: Util.even?(8), is_equal_to: true
+  check that: Util.even?(16), is_equal_to: true
+  check that: Util.even?(-16), is_equal_to: true
+  check that: Util.even?(1), is_equal_to: false
+  check that: Util.even?(5), is_equal_to: false
+  check that: Util.even?(-5), is_equal_to: false
 end
