201f437dc2a996592a95ae3e5cb0c50f95547c78
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-20 21:41:45 +0200

    exercises/basics/09-multiple-clauses

--- exercises/basics/09-multiple-clauses/tests.exs
+++ exercises/basics/09-multiple-clauses/tests.exs
@@ -4,20 +4,25 @@ file = System.get_env("DA_TESTFILE") || "student.exs"
 Code.load_file(file, __DIR__)
 
 
+defmodule Aux do
+  defmacro check(that: block, is_equal_to: expected) do
+    str = Macro.to_string(block)
+    quote do
+      test "#{unquote(str)} should be equal to #{unquote(expected)}" do
+        assert unquote(block) == unquote(expected)
+      end
+    end
+  end
+end
+
+
 defmodule Tests do
   use ExUnit.Case, async: true
+  import Aux
 
-  data = [
-    { 0, 0 },
-    { 1, 1 },
-    { -1, 1 },
-    { 2, 2 },
-    { -2, 2 },
-  ]
-
-  data |> Enum.each( fn {input, expected} ->
-    test "Absolute value of #{input} should equal #{expected}" do
-      assert Numbers.abs(unquote(input)) == unquote(expected)
-    end
-  end )
+  check that: abs(0), is_equal_to: 0
+  check that: abs(1), is_equal_to: 1
+  check that: abs(-1), is_equal_to: 1
+  check that: abs(2), is_equal_to: 2
+  check that: abs(-2), is_equal_to: 2
 end
