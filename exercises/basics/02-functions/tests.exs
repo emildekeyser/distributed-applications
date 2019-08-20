80ac6aa3bd7392eb939e28dd40ed284d101ac258
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-20 21:47:54 +0200

    exercises/basics/02-functions

--- exercises/basics/02-functions/tests.exs
+++ exercises/basics/02-functions/tests.exs
@@ -3,26 +3,27 @@ ExUnit.start()
 file = System.get_env("DA_TESTFILE") || "student.exs"
 Code.load_file(file, __DIR__)
 
+defmodule Aux do
+  defmacro check(that: block, is_equal_to: expected) do
+    str = Macro.to_string(block)
+    quote do
+      test "#{unquote(str)} should be equal to #{unquote(expected)}" do
+        assert unquote(block) == unquote(expected)
+      end
+    end
+  end
+end
+
 
 defmodule Tests do
   use ExUnit.Case, async: true
+  import Aux
 
-  data = [
-    { -273.15, 0 },
-    { 0, 273.15 },
-    { 10, 283.15 },
-  ]
+  check(that: Temperature.kelvin_to_celsius(0), is_equal_to: -273.15)
+  check(that: Temperature.kelvin_to_celsius(273.15), is_equal_to: 0)
+  check(that: Temperature.kelvin_to_celsius(283.15), is_equal_to: 10)
 
-  for { celsius, kelvin } <- data do
-    @celsius celsius
-    @kelvin kelvin
-
-    test "#{kelvin}K equals #{celsius}C" do
-      assert Temperature.kelvin_to_celsius(@kelvin) == @celsius
-    end
-
-    test "#{celsius}C equals #{kelvin}K" do
-      assert Temperature.celsius_to_kelvin(@celsius) == @kelvin
-    end
-  end
+  check(that: Temperature.celsius_to_kelvin(-273.15), is_equal_to: 0)
+  check(that: Temperature.celsius_to_kelvin(0), is_equal_to: 273.15)
+  check(that: Temperature.celsius_to_kelvin(10), is_equal_to: 283.15)
 end
