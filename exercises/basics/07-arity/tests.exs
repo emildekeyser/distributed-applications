154ce792a0adcbde011d66069c95ebb9ccb12c06
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-20 22:29:12 +0200

    exercises/basics/07-arity

--- exercises/basics/07-arity/tests.exs
+++ exercises/basics/07-arity/tests.exs
@@ -3,25 +3,31 @@ ExUnit.start()
 file = System.get_env("DA_TESTFILE") || "student.exs"
 Code.load_file(file, __DIR__)
 
+defmodule Aux do
+  defmacro check(that: block, is_equal_to: expected) do
+    str = Macro.to_string(block)
+    quote do
+      test "#{unquote(str)} should be equal to #{unquote(expected)}" do
+        assert unquote(block) == unquote(expected)
+      end
+    end
+  end
+end
 
 defmodule Tests do
   use ExUnit.Case, async: true
+  import Aux
 
-  data2 = [
-    { 1, 2, 2 },
-    { 2, 1, 2 },
-    { 1, 1, 1 },
-    { 4, 3, 4 },
-    { 1, 8, 8 },
-  ]
+  check that: Max.maximum(1, 2), is_equal_to: 2
+  check that: Max.maximum(3, 2), is_equal_to: 3
+  check that: Max.maximum(1, 5, 2), is_equal_to: 5
 
-  for { a, b, expected } <- data2 do
-    @a a
-    @b b
-    @expected expected
+  check that: Max.maximum(7, 5, 2), is_equal_to: 7
+  check that: Max.maximum(7, 5, 9), is_equal_to: 9
+  check that: Max.maximum(7, 10, 9), is_equal_to: 10
 
-    test "maximum(#{@a}, #{@b}) should equal #{@expected}" do
-      assert Max.maximum(@a, @b) == @expected
-    end
-  end
+  check that: Max.maximum(3, 2, 1, 0), is_equal_to: 3
+  check that: Max.maximum(3, 4, 1, 0), is_equal_to: 4
+  check that: Max.maximum(3, 4, 7, 0), is_equal_to: 7
+  check that: Max.maximum(3, 4, 7, 9), is_equal_to: 9
 end
