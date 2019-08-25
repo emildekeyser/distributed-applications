2aa96633dc51ff31c72908f958af44a4b7cb49f0
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-25 23:00:24 +0200

    exercises/basics/08-guards

--- exercises/basics/08-guards/solution.exs
+++ exercises/basics/08-guards/solution.exs
@@ -1,9 +1,4 @@
-defmodule Util do
-  def even?(n) when is_number(n) do
-    rem(n, 2) == 0
-  end
-
-  def odd?(n) when is_number(n) do
-    rem(n, 2) != 0
-  end
+defmodule Numbers do
+  def even?(n) when is_number(n), do: rem(n, 2) == 0
+  def odd?(n) when is_number(n), do: rem(n, 2) != 0
 end
