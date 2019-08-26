d8e1aba2ccb3a04afcbcc0280557f1ddff4b42bb
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-26 21:09:54 +0200

    exercises/data-types/01-atoms

--- exercises/data-types/01-atoms/tests.exs
+++ exercises/data-types/01-atoms/tests.exs
@@ -1,37 +1,45 @@
-ExUnit.start()
+defmodule Setup do
+  @script "shared.exs"
 
-file = System.get_env("DA_TESTFILE") || "student.exs"
-Code.load_file(file, __DIR__)
+  def setup(directory \\ ".") do
+    path = Path.join(directory, @script)
+
+    if File.exists?(path) do
+      Code.require_file(path)
+      Shared.setup(__DIR__)
+    else
+      setup(Path.join(directory, ".."))
+    end
+  end
+end
+
+Setup.setup
 
 
 defmodule Tests do
   use ExUnit.Case, async: true
+  import Shared
 
-  data = [
-    { :ace, :ace, false },
-    { 2, :ace, false },
-    { :ace, 2, true },
-    { 4, 8, false },
-    { 5, 2, true },
-    { :jack, :queen, false },
-    { :queen, :jack, true },
-    { :jack, :king, false },
-    { :king, :jack, true },
-    { :king, :queen, true },
-    { :queen, :king, false },
-    { :king, :ace, false },
-    { :ace, :king, true },
-    { :jack, 10, true },
-    { 5, :queen, false },
-  ]
 
-  for { x, y, expected } <- data do
-    @x x
-    @y y
-    @expected expected
+  check that: Cards.higher?(7, 5), is_equal_to: true
+  check that: Cards.higher?(10, 2), is_equal_to: true
+  check that: Cards.higher?(:king, 10), is_equal_to: true
+  check that: Cards.higher?(:ace, :king), is_equal_to: true
+  check that: Cards.higher?(:king, :queen), is_equal_to: true
+  check that: Cards.higher?(:queen, :jack), is_equal_to: true
+  check that: Cards.higher?(:king, :jack), is_equal_to: true
+  check that: Cards.higher?(:jack, 10), is_equal_to: true
+  check that: Cards.higher?(:ace, 2), is_equal_to: true
+  check that: Cards.higher?(3, 2), is_equal_to: true
 
-    test "higher?(#{@x}, #{@y}) -> #{@expected}" do
-      assert Cards.higher?(@x, @y) === @expected
-    end
-  end
+  check that: Cards.higher?(2, 2), is_equal_to: false
+  check that: Cards.higher?(2, :ace), is_equal_to: false
+  check that: Cards.higher?(4, :ace), is_equal_to: false
+  check that: Cards.higher?(:ace, :ace), is_equal_to: false
+  check that: Cards.higher?(:king, :ace), is_equal_to: false
+  check that: Cards.higher?(:king, :king), is_equal_to: false
+  check that: Cards.higher?(:queen, :ace), is_equal_to: false
+  check that: Cards.higher?(:jack, :ace), is_equal_to: false
+  check that: Cards.higher?(10, :ace), is_equal_to: false
+  check that: Cards.higher?(8, 10), is_equal_to: false
 end
