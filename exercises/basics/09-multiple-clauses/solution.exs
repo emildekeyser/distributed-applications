4be139cd788c7bf41847da187e5bbbde6f49c7fa
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-26 11:24:12 +0200

    exercises/basics/09-multiple-clauses

--- exercises/basics/09-multiple-clauses/solution.exs
+++ exercises/basics/09-multiple-clauses/solution.exs
@@ -1,4 +1,4 @@
 defmodule Numbers do
-  def abs(n) when n >= 0, do: n
-  def abs(n) when n < 0, do: -n
+  def abs(n) when is_number(n) and n >= 0, do: n
+  def abs(n) when is_number(n) and n < 0, do: -n
 end
