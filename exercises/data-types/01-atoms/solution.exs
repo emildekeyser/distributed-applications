5fa3c1e8ca97c1fe7d8fb84766194f3ac20f5944
Frederic Vogels <frederic.vogels@ucll.be>
2019-08-27 11:27:13 +0200

    exercises/data-types/01-atoms

--- exercises/data-types/01-atoms/solution.exs
+++ exercises/data-types/01-atoms/solution.exs
@@ -1,9 +1,9 @@
 defmodule Cards do
-  defp value(k) when is_number(k), do: k
-  defp value(:jack), do: 11
-  defp value(:queen), do: 12
-  defp value(:king), do: 13
-  defp value(:ace), do: 14
+  defp numericValue(k) when is_number(k), do: k
+  defp numericValue(:jack), do: 11
+  defp numericValue(:queen), do: 12
+  defp numericValue(:king), do: 13
+  defp numericValue(:ace), do: 14
 
-  def higher?(x, y), do: value(x) > value(y)
+  def higher?(x, y), do: numericValue(x) > numericValue(y)
 end
