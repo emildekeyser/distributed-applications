8eec31f0ee0d865764610068a941aac97a59947f
Frederic Vogels <frederic.vogels@ucll.be>
2019-09-03 09:00:55 +0200

    cleanup in first class functions directory

--- exercises/03-higher-order-functions/01-first-class-functions/compose/solution.exs
+++
@@ -1,5 +0,0 @@
-defmodule Functions do
-  def compose(f, g) do
-    fn x -> g.(f.(x)) end
-  end
-end
--- exercises/03-higher-order-functions/01-first-class-functions/nest/solution.exs
+++
@@ -1,4 +0,0 @@
-defmodule Functions do
-  def nest(_, 0, x), do: x
-  def nest(f, k, x), do: nest(f, k - 1, f.(x))
-end
