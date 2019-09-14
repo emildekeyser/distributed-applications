5256cf902add86ee3b48179069c13dfba6220dec
Frederic Vogels <frederic.vogels@ucll.be>
2019-09-14 18:35:13 +0200

    slides/processes

--- slides/processes/message-passing.exs
+++ slides/processes/message-passing.exs
@@ -1,13 +1,13 @@
-defp child_ai() do
-  IO.puts("Are were there yet?")
+defp `\cnode{child1}`child_ai`\cnode{child2}`() do
+  IO.`\cnode{annoy1}`puts`\cnode{annoy2}`("Are were there yet?")
 
-  receive do
-    :arrived -> IO.puts("Finally!")
+  `\cnode{receive1}`receive`\cnode{receive2}` do
+    `\cnode{msg1}`:arrived`\cnode{msg2}` -> IO.`\cnode{finally1}`puts`\cnode{finally2}`("Finally!")
   after
-    0 -> child_ai()
+    `\cnode{timeout1}`1`\cnode{timeout2}` -> `\cnode{rec1}`child_ai`\cnode{rec2}`()
   end
 end
 
-child = spawn(&child_ai/0)
-drive()
-send(child_ai, :arrived)
+child = `\cnode{spawn1}`spawn`\cnode{spawn2}`(&child_ai/0)
+`\cnode{drive1}`drive`\cnode{drive2}`()
+`\cnode{send1}`send`\cnode{send2}`(child_ai, :arrived)
